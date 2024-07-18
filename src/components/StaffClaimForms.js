67f94ef4394419ea1032942e708fbde09b10c9dd
Ashish <kumarashish98526@gmail.com>
2024-07-18 15:00:21 +0530

    modies links of some files

--- src/components/StaffClaimForms.js
+++ src/components/StaffClaimForms.js
@@ -6,126 +6,125 @@ import { TabPage } from "./styles/tabpage"
 const SCF = {
  title: <h1>Staff Claim Form</h1>,
  content: [
-  {
-   Downloads: "Advance Form",
-   DLink: `${home}/staffForm/Advance form.pdf`,
-   "Office order": "Medical Notice",
-   OLink: `${home}/staffForm/Medical notice.pdf`,
-  },
-  {
-   Downloads: "CEA Form",
-   DLink: `${home}/staffForm/CEA form.pdf`,
-   "Office order": "Office Order",
-   OLink: `${home}/staffForm/office order.pdf`,
-  },
-  {
-   Downloads: "CL Application",
-   DLink: `${home}/staffForm/1.%20Casual%20Leave%20(CL),%20Restricted%20Leave%20(RH).pdf`,
-   "Office order": "Bills/PDA form",
-   OLink: `${home}/staffForm/Circular5c370f6512525.pdf`,
-  },
-  {
-   Downloads: "Restricted Leave  (RH)",
-   DLink: `${home}/staffForm/1.%20Casual%20Leave%20(CL),%20Restricted%20Leave%20(RH).pdf`,
-   "Office order": "",
-  },
-  {
-   Downloads: "El Form",
-   DLink: `${home}/staffForm/2.%20Earned%20Leave%20(EL).pdf`,
-   //   "Office order": "New Bill Format",
-   //   OLink: `${home}/staffForm/Bill_format.pdf`,
-  },
-  {
-   Downloads: "Child Care Leave (CCL) ",
-   DLink: `${home}/staffForm/3.%20Child%20Care%20Leave.pdf`,
-   "Office order": "Bill Format",
-   // OLink: `${home}/staffForm/Bill%20format%20dt%2019%20Apr%202023.pdf`,
-   OLink: `${home}/staffForm/bill%20format%20new%20bilingual.docx.pdf`,
-  },
-  {
-   Downloads: "Commuted Leave",
-   DLink: `${home}/staffForm/4.%20Commuted,%20HPL,%20Maternity,%20Paternity%20Leave.pdf`,
-   "Office order": "",
-  },
-  {
-   Downloads: "Half Pay Leave (HPL) ",
-   DLink: `${home}/staffForm/4.%20Commuted,%20HPL,%20Maternity,%20Paternity%20Leave.pdf`,
-   "Office order": "Guest House Requisition Form",
-   OLink: `https://drive.google.com/file/d/1CLiYVUjw7VnOgFq4W-M6Ext7Ccu8XRz2/view`,
-  },
-  {
-   Downloads: "Maternity Leave",
-   DLink: `${home}/staffForm/4.%20Commuted,%20HPL,%20Maternity,%20Paternity%20Leave.pdf`,
-   "Office order": "",
-  },
-  {
-   Downloads: "Paternity Leave",
-   DLink: `${home}/staffForm/4.%20Commuted,%20HPL,%20Maternity,%20Paternity%20Leave.pdf`,
-   "Office order": "",
-  },
-  {
-   Downloads: "Leave Encashment (LTC)",
-   DLink: `${home}/staffForm/6.%20Leave%20Encashment%20(LTC).pdf`,
-   "Office order": "",
-  },
-  {
-   Downloads: "LTC Application Form",
-   DLink: `${home}/staffForm/5.%20Application%20for%20LTC%20(Home%20town,%20All%20India).pdf`,
-   "Office order": "LTC Claim Form",
-   OLink: `${home}/staffForm/ltcclaim_updated.pdf`,
-  },
-  {
-   Downloads: "Medical Claim Form",
-   DLink: `${home}/staffForm/claim.pdf`,
-   "Office order": "List of Hospitals",
-   OLink: `${home}/staffForm/list_of_hospitals.pdf`,
-  },
-  {
-   Downloads: "Medical Certificate (A)",
-   DLink: `${home}/staffForm/Medical Certificate (A).pdf`,
-   "Office order": "",
-  },
-  {
-   Downloads: "Medical Certificate (B)",
-   DLink: `${home}/staffForm/Medical Certificate (B).pdf`,
-   "Office order": "",
-  },
-  {
-   Downloads: "Medical Emergency Certificate",
-   DLink: `${home}/staffForm/Emergency CERTIFICATE.pdf`,
-   "Office order": "",
-  },
-  {
-   Downloads: "Medical Nursing Certificate",
-   DLink: `${home}/staffForm/Nursing CERTIFICATE.pdf`,
-   "Office order": "",
-  },
-  {
-   Downloads: "NPS Registration Form",
-   DLink: `${home}/staffForm/Subscriber_Registration_Form_CSRF.pdf`,
-   "Office order": "",
-  },
-  {
-   Downloads: "T.A. For Expert",
-   DLink: `${home}/staffForm/T.A. for expert.pdf`,
-   "Office order": "",
-  },
-  {
-   Downloads: "Travel Bill For Tour",
-   DLink: `${home}/staffForm/travallbillfortour_updated.pdf`,
-   "Office order": "",
-  },
-  {
-   Downloads: "Vehicle Permission Form",
-   DLink: `${home}/staffForm/Vehicle_Permission.pdf`,
-   "Office order": "",
-  },
-  {
-   Downloads: "Security Gate Pass",
-   DLink: `${home}/staffForm/Security_gate_Pass.pdf`,
-   "Office order": "",
-  },
- ],
+    {
+      "Downloads": "Advance Form",
+      "DLink": "https://drive.google.com/file/d/1_C08oxqao4yYfHsQo6q7tA9am99FIbpF/view?usp=drivesdk",
+      "Office order": "Medical Notice",
+      "OLink": "https://drive.google.com/file/d/1EwZ0Lsf2ASCbUKrJtQSrK_qLaqECGM0I/view?usp=drivesdk"
+    },
+    {
+      "Downloads": "CEA Form",
+      "DLink": "https://drive.google.com/file/d/133utXZl2AyKBzX-1h-cHXfOixHWgCugx/view?usp=drivesdk",
+      "Office order": "Office Order",
+      "OLink": "https://drive.google.com/file/d/1GlngSIeMQYW67OspVNhQ0MowHxyUUGo_/view?usp=drivesdk"
+    },
+    {
+      "Downloads": "CL Application",
+      "DLink": "https://drive.google.com/file/d/1tgRaUy-dOwX317GOhpTHE-LpUr3KdSQo/view?usp=drivesdk",
+      "Office order": "Bills/PDA form",
+      "OLink": "https://drive.google.com/file/d/12WJeTf291spsAq3Cu40i02nDcex5CQu4/view?usp=drivesdk"
+    },
+    {
+      "Downloads": "Restricted Leave (RH)",
+      "DLink": "https://drive.google.com/file/d/1HIQ-qcSpzOTwxixl1VUUQzFrRBtSESXs/view?usp=drivesdk",
+      "Office order": ""
+    },
+    {
+      "Downloads": "El Form",
+      "DLink": "https://drive.google.com/file/d/1XdOjVJjjQgJTUGpuwBTFy_jFCrUrDfdX/view?usp=drivesdk",
+      "Office order": ""
+    },
+    {
+      "Downloads": "Child Care Leave (CCL)",
+      "DLink": "https://drive.google.com/file/d/1dhS2vd6kpp7ZG7jyc8NrbpHW_s-xTN1B/view?usp=drivesdk",
+      "Office order": "Bill Format",
+      "OLink": "https://drive.google.com/file/d/13LB_4cl49dLuZAmo6RrkR5hpQPqB6KUT/view?usp=drivesdk"
+    },
+    {
+      "Downloads": "Commuted Leave",
+      "DLink": "https://drive.google.com/file/d/1kmmqvoyWDsiv6-VWI75_R944HYfLmzwP/view?usp=drivesdk",
+      "Office order": ""
+    },
+    {
+      "Downloads": "Half Pay Leave (HPL)",
+      "DLink": "https://drive.google.com/file/d/15IqlRuaWyh9TGbwBCholovTUY049eBHS/view?usp=drivesdk",
+      "Office order": "Guest House Requisition Form",
+      "OLink": "https://drive.google.com/file/d/1Wu8JtnAgpKUGXHQZ9XXLriiTHqHOZQNm/view?usp=drivesdk"
+    },
+    {
+      "Downloads": "Maternity Leave",
+      "DLink": "https://drive.google.com/file/d/1VvsiQMLQS15jvT8MAQA3Y_AdaCXbdAHw/view?usp=drivesdk",
+      "Office order": ""
+    },
+    {
+      "Downloads": "Paternity Leave",
+      "DLink": "https://drive.google.com/file/d/1QwgUuYRv7ALfR9GN8MLO313MuoQNzEZU/view?usp=drivesdk",
+      "Office order": ""
+    },
+    {
+      "Downloads": "Leave Encashment (LTC)",
+      "DLink": "https://drive.google.com/file/d/1dWXmpjqVtcUhzjOM_rkuEEv8s7mGQ_il/view?usp=drivesdk",
+      "Office order": ""
+    },
+    {
+      "Downloads": "LTC Application Form",
+      "DLink": "https://drive.google.com/file/d/1AYfw5ZIovBJmR0Y2BszAHRJwfvItrNMx/view?usp=drivesdk",
+      "Office order": "LTC Claim Form",
+      "OLink": "https://drive.google.com/file/d/1ZAHxlpCd7LCxdxb0JBLyZEbuhw6V2UI7/view?usp=drivesdk"
+    },
+    {
+      "Downloads": "Medical Claim Form",
+      "DLink": "https://drive.google.com/file/d/1Rx2FxIqoWBsxqFbmBTvWCl52ZLN_DfMy/view?usp=drivesdk",
+      "Office order": "List of Hospitals",
+      "OLink": "https://drive.google.com/file/d/1YC839XCnkbgAfpW9pFLqko6t135i1kD1/view?usp=drivesdk"
+    },
+    {
+      "Downloads": "Medical Certificate (A)",
+      "DLink": "https://drive.google.com/file/d/1pHaYlHR2PJW29pUAfaLTJ_fnoEOuu4Iv/view?usp=drivesdk",
+      "Office order": ""
+    },
+    {
+      "Downloads": "Medical Certificate (B)",
+      "DLink": "https://drive.google.com/file/d/1hXLAzspVCDdoQt9pKQ9WuTCpOOQZPZTu/view?usp=drivesdk",
+      "Office order": ""
+    },
+    {
+      "Downloads": "Medical Emergency Certificate",
+      "DLink": "https://drive.google.com/file/d/1K2Cjbiuhl9_X-vyvWwxqA324ry5ViiGX/view?usp=drivesdk",
+      "Office order": ""
+    },
+    {
+      "Downloads": "Medical Nursing Certificate",
+      "DLink": "https://drive.google.com/file/d/1MUC3NgNuUT8ebaGbIx1qTRsJvMfp-KPG/view?usp=drivesdk",
+      "Office order": ""
+    },
+    {
+      "Downloads": "NPS Registration Form",
+      "DLink": "https://drive.google.com/file/d/1mU8pg2dtT4ujCSYeFgtcQDh1EYg80vxD/view?usp=drivesdk",
+      "Office order": ""
+    },
+    {
+      "Downloads": "T.A. For Expert",
+      "DLink": "https://drive.google.com/file/d/1J58gXeGKQ4SHjRGVi-zKjmCdJF0dQrHD/view?usp=drivesdk",
+      "Office order": ""
+    },
+    {
+      "Downloads": "Travel Bill For Tour",
+      "DLink": "https://drive.google.com/file/d/1sNIeZaxqsmYDE5kFTXp13_QiXVPHRu1x/view?usp=drivesdk",
+      "Office order": ""
+    },
+    {
+      "Downloads": "Vehicle Permission Form",
+      "DLink": "https://drive.google.com/file/d/1azIfinOBLGbKWTMekJKd6HDvm_Ox5tlJ/view?usp=drivesdk",
+      "Office order": ""
+    },
+    {
+      "Downloads": "Security Gate Pass",
+      "DLink": "https://drive.google.com/file/d/167jo8PmiS5lR5bhe9fGDwbadWr-Lvb28/view?usp=drivesdk",
+      "Office order": ""
+    }
+  ]
+  
 }
 
 const StaffClaimForms = () => {
--- src/components/academics/acadData.js
+++ src/components/academics/acadData.js
@@ -597,7 +597,7 @@ const Fee = [
    {
     para: "UG : Fee refund policy notification",
     link:
-     "https://web.nitp.ac.in//uploads23/UG%20Refund%20Policy%20Notification.pdf",
+     "https://drive.google.com/file/d/1C2CcX99So6oIYAd1a7npa99swZwWwlel/view?usp=drive_link",
    },
    {
     para:
@@ -616,7 +616,7 @@ const Fee = [
    {
     para:
      "UG/PG Dual Degree: Fee notification for B.Tech and M.Tech Dual Degree",
-    link: "http://web.nitp.ac.in/uploads22/FeeNotification_Dual.pdf",
+    link: "https://drive.google.com/file/d/1c9ZFzCTiu-hLniL0gtcvhS-uvsEuROxM/view?usp=drive_link",
    },
   ],
  },
@@ -640,87 +640,87 @@ const Fee = [
 const Format = [
  {
   heading: "Format",
-  data: [
+  data:[
     
-   {
-    para: "Guidelines cum checklist to get process of academic document",
-    link:
-     "http://web.nitp.ac.in/downloads/Guidelines%20cum%20checklist%20to%20get%20process%20of%20academic%20document.pdf",
-   },
-   {
-    para:
-     "Format for issue of Grade Card / Provisional / Transcript from Exam Section",
-    link:
-     
-     "https://drive.google.com/file/d/1AhtQCtdBLB-FG-JldD3_j8NPTsj4EONp/view?usp=drivesdk",
-   },
-   {
-    para: "Attestation form for grade cards",
-    link:
-     
-     "https://drive.google.com/file/d/1AjHBBA3ca9s-T_jgtocOz5EYaJOzhbHa/view?usp=drivesdk",
-   },
-   {
-    para: "1-SOP to issue Original Degree Certificate",
-   
-    link:
-     "https://drive.google.com/file/d/1ojThnsLJgSUHY4Zr0QCTGHdxOUvLfY6t/view?usp=sharing",
-   },
-   {
-    para: "2-SOP to issue Institute Leaving/Transfer Cum Migration Certificate",
+    {
+     para: "Guidelines cum checklist to get process of academic document",
+     link:
+      "https://drive.google.com/file/d/1RWpLyN6uVWrvfQBfbhKstRRXEVsNLejk/view?usp=drive_link",
+    },
+    {
+     para:
+      "Format for issue of Grade Card / Provisional / Transcript from Exam Section",
+     link:
+      
+      "https://drive.google.com/file/d/1AhtQCtdBLB-FG-JldD3_j8NPTsj4EONp/view?usp=drivesdk",
+    },
+    {
+     para: "Attestation form for grade cards",
+     link:
+      
+      "https://drive.google.com/file/d/1AjHBBA3ca9s-T_jgtocOz5EYaJOzhbHa/view?usp=drivesdk",
+    },
+    {
+     para: "1-SOP to issue Original Degree Certificate",
     
-    link:
-     "https://drive.google.com/file/d/1AymHBdkagkjfUfklSer4pg-3iaqlXhjN/view?usp=sharing",
-   },
-   {
-    para:
-     "3-SOP for Refund of DRCC Education Loan under Bihar Student Credit Card Scheme",
- 
-    link:
-     "https://drive.google.com/file/d/1KXs3dru2FJZ5q_kV1h36TK6E7EkOO0n9/view?usp=drivesdk",
-   },
-   {
-    para:
-     "4-SOP for Adjustment of DRCC Education Loan under Bihar Student Credit Card Scheme",
+     link:
+      "https://drive.google.com/file/d/1ojThnsLJgSUHY4Zr0QCTGHdxOUvLfY6t/view?usp=sharing",
+    },
+    {
+     para: "2-SOP to issue Institute Leaving/Transfer Cum Migration Certificate",
+     
+     link:
+      "https://drive.google.com/file/d/1AymHBdkagkjfUfklSer4pg-3iaqlXhjN/view?usp=sharing",
+    },
+    {
+     para:
+      "3-SOP for Refund of DRCC Education Loan under Bihar Student Credit Card Scheme",
   
-    link:
-     "https://drive.google.com/file/d/1t7NsBr6xqNqgUyuuxW-l-vFE35zrc3vi/view?usp=sharing",
-   },
-   {
-
-    para: "5-SOP for Educational Verification",
+     link:
+      "https://drive.google.com/file/d/1KXs3dru2FJZ5q_kV1h36TK6E7EkOO0n9/view?usp=drivesdk",
+    },
+    {
+     para:
+      "4-SOP for Adjustment of DRCC Education Loan under Bihar Student Credit Card Scheme",
    
-    link:
-     " https://drive.google.com/file/d/1m5VB8SMkkllmXRZjaxWi8xc8E7PMGfkB/view?usp=sharing",
-   },
-   {
+     link:
+      "https://drive.google.com/file/d/1t7NsBr6xqNqgUyuuxW-l-vFE35zrc3vi/view?usp=sharing",
+    },
+    {
+ 
+     para: "5-SOP for Educational Verification",
     
-    para: "6-Format of Application to Dean(Academic)",
-    
-    link:
-     "https://drive.google.com/file/d/19ZBDsaDluGRBJO-LgBtdKYqo_8CQrbmx/view?usp=sharing",
-   },
-   {
-
-    para: "7-Format of Application for Demand Letter",
-    
-    link:
-     "https://drive.google.com/file/d/18F1po6Nxh7ashYBG4C4A9exOaaB5-Jy2/view?usp=sharing",
-   },
-   {
-
-    para:
-     "8-Format of Application for Fee Receipt for Bank Loan and DRCC Adjustment Only",
-    
-    link:
-     "https://drive.google.com/file/d/1XHKMmDhheGwL67m-q42FqPMAU-a4X0Hu/view?usp=sharing",
-   },
-   {
-    para: "9-Format of Application for Cancellation of Admission",
-    link:
-     "https://drive.google.com/file/d/1p60py5fKWJ_iWn9s17NCtStlhfOTRqea/view?usp=sharing",
-   },
-  ],
+     link:
+      " https://drive.google.com/file/d/1m5VB8SMkkllmXRZjaxWi8xc8E7PMGfkB/view?usp=sharing",
+    },
+    {
+     
+     para: "6-Format of Application to Dean(Academic)",
+     
+     link:
+      "https://drive.google.com/file/d/19ZBDsaDluGRBJO-LgBtdKYqo_8CQrbmx/view?usp=sharing",
+    },
+    {
+ 
+     para: "7-Format of Application for Demand Letter",
+     
+     link:
+      "https://drive.google.com/file/d/18F1po6Nxh7ashYBG4C4A9exOaaB5-Jy2/view?usp=sharing",
+    },
+    {
+ 
+     para:
+      "8-Format of Application for Fee Receipt for Bank Loan and DRCC Adjustment Only",
+     
+     link:
+      "https://drive.google.com/file/d/1XHKMmDhheGwL67m-q42FqPMAU-a4X0Hu/view?usp=sharing",
+    },
+    {
+     para: "9-Format of Application for Cancellation of Admission",
+     link:
+      "https://drive.google.com/file/d/1p60py5fKWJ_iWn9s17NCtStlhfOTRqea/view?usp=sharing",
+    },
+   ]
  },
 ]
 const CourseUG = [
