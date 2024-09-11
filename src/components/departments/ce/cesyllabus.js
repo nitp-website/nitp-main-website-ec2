75266b08a85459553351592ed812244baebb26cc
Ashish <kumarashish98526@gmail.com>
2024-09-11 22:43:44 +0530

    "Updated links and file names in acadData.js and cesyllabus.js to point to Google Drive files and removed file extensions from link text."

--- src/components/departments/ce/cesyllabus.js
+++ src/components/departments/ce/cesyllabus.js
@@ -66,7 +66,7 @@ const Cesyllabus = () => {
               <ul>
                 <li>
                   <a href="https://drive.google.com/file/d/1TTblz48XLiUYs5JRHlqa37VWqpyWXb_w/view?usp=drivesdk">
-                  B.Tech Civil Engineering (Effective from 2022-2023 onward).pdf
+                  B.Tech Civil Engineering (Effective from 2022-2023 onward)
                   </a>
                 </li>
               </ul>
@@ -77,27 +77,27 @@ const Cesyllabus = () => {
               <ul>
               <li>
                 <a href="https://drive.google.com/file/d/16XTey9OYvtOJFERcuBfNXlzZU_NskNEg/view?usp=drivesdk">
-                  M.Tech Water Resources Engineering (Effective from 2021-2022 onward).pdf
+                  M.Tech Water Resources Engineering (Effective from 2021-2022 onward)
                 </a>
               </li>
               <li>
                 <a href="https://drive.google.com/file/d/1fXR4RROFC2UQo461jiNn7--ZQJoN384i/view?usp=drivesdk">
-                  M.Tech Transportation Engineering (Effective from 2021-2022 onward).pdf
+                  M.Tech Transportation Engineering (Effective from 2021-2022 onward)
                 </a>
               </li>
               <li>
                 <a href="https://drive.google.com/file/d/1q8Wndh5pM4EcILaTqfafpnag20XqOtS6/view?usp=drivesdk">
-                  M.Tech Structural Engineering (Effective from 2021-2022 onward).pdf
+                  M.Tech Structural Engineering (Effective from 2021-2022 onward)
                 </a>
               </li>
               <li>
                 <a href="https://drive.google.com/file/d/1-r7SS3hmSbMl52e2VIqJnti8yPlLWiLa/view?usp=drivesdk">
-                  M.Tech Geotechnical Engineering (Effective from 2021-2022 onward).pdf
+                  M.Tech Geotechnical Engineering (Effective from 2021-2022 onward)
                 </a>
               </li>
               <li>
                 <a href="https://drive.google.com/file/d/1X57Lq9nCBjK7Db8m990hYBuj_81tRfkE/view?usp=drivesdk">
-                  M.Tech Environmental Engineering (Effective from 2021-2022 onward).pdf
+                  M.Tech Environmental Engineering (Effective from 2021-2022 onward)
                 </a>
               </li>
             </ul>
@@ -114,7 +114,7 @@ const Cesyllabus = () => {
               <ul>
                 <li>
                   <a href="https://drive.google.com/file/d/1jsKFA6qu4OGyIJSAf8Bp6Ow8Wz2houq0/view?usp=drivesdk">
-                    Dual Degree (B.Tech (Civil Engineering) + M.Tech (Construction Technology Management)) Effective from 2022-2023 onward.pdf
+                    Dual Degree (B.Tech (Civil Engineering) + M.Tech (Construction Technology Management)) Effective from 2022-2023 onward
                   </a>
                 </li>
               </ul>
